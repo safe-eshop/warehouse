e039d12f733f5eb7e10f7d91ec710da1ef9c9c05
dominikus1993 <dominikus1910@gmail.com>
2019-11-15 22:47:58 +0100

    reorganize folders

--- app/domain/model/model.go
+++ app/domain/model/model.go
@@ -1,4 +1,4 @@
-package domain
+package model
 
 type WarehouseState struct {
 	CatalogItemId string `json:"catalogItemId,omitempty"`
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"rossmann/domain"
+	"rossmann/app/domain/model"
 )
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/ping/:quantity", func(c *gin.Context) {
-		stock := domain.NewWarehouseState("hagjhdsag", 2, 4)
+		stock := model.NewWarehouseState("hagjhdsag", 2, 4)
 		c.JSON(200, stock)
 	})
 	_ = r.Run("0.0.0.0:9000") // listen and serve on 0.0.0.0:8080
